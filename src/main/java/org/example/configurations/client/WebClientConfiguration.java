4043f5550a99b4637b516dd839778ff0049fd7bc
Alexander Afonin <63101279+lueFlake@users.noreply.github.com>
2024-04-12 13:41:24 +0300

    fixed buffer exception

--- src/main/java/org/example/configurations/client/WebClientConfiguration.java
+++ src/main/java/org/example/configurations/client/WebClientConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
 import reactor.netty.tcp.TcpClient;
@@ -43,10 +44,7 @@ public class WebClientConfiguration {
                     connection.addHandlerLast(new WriteTimeoutHandler(LONG_TIMEOUT_MILLIS, TimeUnit.MILLISECONDS));
                 });
 
-        return WebClient.builder()
-                .baseUrl(ATLAS_BASE_URL)
-                .clientConnector(new ReactorClientHttpConnector(HttpClient.from(tcpClient)))
-                .build();
+        return buildFromTcpClient(tcpClient, ATLAS_BASE_URL);
     }
 
     @Bean
@@ -60,10 +58,7 @@ public class WebClientConfiguration {
                     connection.addHandlerLast(new WriteTimeoutHandler(TIMEOUT, TimeUnit.MILLISECONDS));
                 });
 
-        return WebClient.builder()
-                .baseUrl(ATLAS_AGENT_BASE_URL)
-                .clientConnector(new ReactorClientHttpConnector(HttpClient.from(tcpClient)))
-                .build();
+        return buildFromTcpClient(tcpClient, ATLAS_AGENT_BASE_URL);
     }
 
     @Bean
@@ -76,11 +71,7 @@ public class WebClientConfiguration {
                     connection.addHandlerLast(new ReadTimeoutHandler(LONG_TIMEOUT_MILLIS, TimeUnit.MILLISECONDS));
                     connection.addHandlerLast(new WriteTimeoutHandler(LONG_TIMEOUT_MILLIS, TimeUnit.MILLISECONDS));
                 });
-
-        return WebClient.builder()
-                .baseUrl(ATLAS_AGENT_BASE_URL)
-                .clientConnector(new ReactorClientHttpConnector(HttpClient.from(tcpClient)))
-                .build();
+        return buildFromTcpClient(tcpClient, ATLAS_AGENT_BASE_URL);
     }
 
     @Bean
@@ -94,8 +85,17 @@ public class WebClientConfiguration {
                     connection.addHandlerLast(new WriteTimeoutHandler(TIMEOUT, TimeUnit.MILLISECONDS));
                 });
 
+        return buildFromTcpClient(tcpClient, KEYCLOAK_BASE_URL);
+    }
+
+    private WebClient buildFromTcpClient(TcpClient tcpClient, String baseUrl) {
+        final int size = 16 * 1024 * 1024;
+        final ExchangeStrategies strategies = ExchangeStrategies.builder()
+                .codecs(codecs -> codecs.defaultCodecs().maxInMemorySize(size))
+                .build();
         return WebClient.builder()
-                .baseUrl(KEYCLOAK_BASE_URL)
+                .exchangeStrategies(strategies)
+                .baseUrl(baseUrl)
                 .clientConnector(new ReactorClientHttpConnector(HttpClient.from(tcpClient)))
                 .build();
     }

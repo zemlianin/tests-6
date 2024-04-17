7427bd4aee5e9859aae0f6b2a601a53643bdd79d
zemlianin <detulie@mail.ru>
2024-04-17 22:37:03 +0300

    f Please enter the commit message for your changes. Lines starting

--- Meta-Api/testing-6/src/main/java/org/example/JazzerTarget.java
+++ Meta-Api/testing-6/src/main/java/org/example/JazzerTarget.java
@@ -5,19 +5,25 @@ import org.example.CalcException;
 
 import com.code_intelligence.jazzer.api.FuzzedDataProvider;
 
+import java.util.ArrayList;
+
 public class JazzerTarget {
     public static void fuzzerTestOneInput(FuzzedDataProvider data){
-        var pipiska = new Pipiska();
         Calc calc = new Calc();
-        var t = data.consumeInt();
-        pipiska.GetSum(t);
 
-        /*String opnstr = "", s = ?;
+        var opnstr = "";
+        var s = data.consumeString(100);
+        var list = new ArrayList<Exception>();
         try {
             opnstr = calc.opn(s);
+        }catch (CalcException ce){
+
         }catch(Exception ce){//используем opn как фильтр, оставляем только корректные выражения
+            list.add(ce);
         }
-        ?*/
+
+        System.out.println(list.size());
+        //?
         //считаем, отсеивая лишнее
     }
 }
